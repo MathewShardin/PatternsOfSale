1495020038d6beb5f9eea0a98a0afd511a32a380
TheLobster1 <rjeveldman@hotmail.nl>
2024-03-21 13:57:23 +0100

    Small update

---
+++ PatternsOfSale/PatternsOfSale/Models/Timer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternsOfSale.Models
+{
+    class Timer : TimeListener
+    {
+        public ArrayList { get; set; }
+        public Timer() {
+            
+        }
+
+        public void UpdateTick(DateTime Tick)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PatternsOfSale/PatternsOfSale/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,9 @@
+namespace PatternsOfSale.ViewModels;
+
+public partial class MainPageViewModel : ViewModel
+{
+	public MainPageViewModel()
+	{
+		
+	}
+}
